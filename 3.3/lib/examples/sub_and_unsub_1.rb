e13534681dce00ee8f15e6d7cb1da15752a9c63e
geremy <geremy@pubnub.com>
2013-05-31 00:26:19 -0700

    removed dump typo

--- 3.3/lib/examples/sub_and_unsub_1.rb
+++ 3.3/lib/examples/sub_and_unsub_1.rb
@@ -5,6 +5,11 @@ cb2 = lambda { |x| puts("cb2 says: #{x}") }
 
 p = Pubnub.new(:subscribe_key => "demo", :publish_key => "demo")
 
-p.subscribe(p.subscribe(:channel => :a, :callback => cb1, :http_sync => true))
+p.subscribe(:channel => "a", :callback => cb1, :http_sync => true)
 sleep(3)
-p.subscribe(p.subscribe(:channel => :ping_3, :callback => cb2, :http_sync => true))
+p.subscribe(:channel => "ping_3", :callback => cb2, :http_sync => true)
+
+while (1) do
+  puts "chilling!"
+  sleep(1)
+end
