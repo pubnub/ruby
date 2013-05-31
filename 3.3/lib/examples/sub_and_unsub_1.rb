d5e0d9300335c4d17402cd6ac349fa353bb764f2
geremy <geremy@pubnub.com>
2013-05-30 23:45:33 -0700

    sub_unsub demo

---
+++ 3.3/lib/examples/sub_and_unsub_1.rb
@@ -0,0 +1,10 @@
+require 'pubnub'
+
+cb1 = lambda { |x| puts("cb1 says: #{x}") }
+cb2 = lambda { |x| puts("cb2 says: #{x}") }
+
+p = Pubnub.new(:subscribe_key => "demo", :publish_key => "demo")
+
+p.subscribe(p.subscribe(:channel => :a, :callback => cb1, :http_sync => true))
+sleep(3)
+p.subscribe(p.subscribe(:channel => :ping_3, :callback => cb2, :http_sync => true))
