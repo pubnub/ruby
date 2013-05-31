6d5bbb8984fa26bf98808efecd9a1dab944eaa26
geremy <geremy@pubnub.com>
2013-05-31 00:58:32 -0700

    more bugs

--- 3.3/lib/examples/sub_and_unsub_1.rb
+++ 3.3/lib/examples/sub_and_unsub_1.rb
@@ -1,21 +1,18 @@
 require 'pubnub'
 
 # I should get a message on channel a once a second, but it doesnt appear so... why not?
-
-
-# x.channel is nil - please fix
-
-cb1 = lambda { |x|
-  puts("cb3 says on channel #{x.channel}: msg: #{x.message}") }
-cb2 = lambda { |x| puts("cb3 says on channel #{x.channel}: msg: #{x.message}") }
-cb3 = lambda { |x| puts("cb3 says on channel #{x.channel}: msg: #{x.message}") }
-
+# x.channel is always nil - please fix
 
 p = Pubnub.new(:subscribe_key => "demo", :publish_key => "demo")
 
+cb1 = lambda { |x|
+  puts("cb1 says on channel #{x.channel}: msg: #{x.message}") }
+cb2 = lambda { |x| puts("cb2 says on channel #{x.channel}: msg: #{x.message}") }
+cb3 = lambda { |x| puts("cb3 says on channel #{x.channel}: msg: #{x.message}") }
+
 p.subscribe(:channel => "a", :callback => cb1, :http_sync => false)
 sleep(3)
-#p.subscribe(:channel => "ping_3", :callback => cb2, :http_sync => false)
+p.subscribe(:channel => "ping_3", :callback => cb2, :http_sync => false)
 
 
 # Provide example of pubnub subcribe with block here
@@ -33,6 +30,8 @@ end
 puts("unsubbing from a")
 p.unsubscribe(:channel => "a", :callback => cb3)
 
+# even after i unsub, im still getting messages (via cb1), why?
+
 x = 10
 
 while (x > 0) do
@@ -47,6 +46,7 @@ end
 x = 10
 while (x > 0) do
 
+  x=x-1
 # provide publish example with block
 
 end
