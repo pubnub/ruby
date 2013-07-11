5691aa95ccf5819c4cd9462d5fcefbe1dac8caf0
Michał Łomnicki <michal.lomnicki@gmail.com>
2013-07-11 15:28:56 +0200

    Allow to pass custom logger
    Current logger always logs to /tmp/pubnubError.log.
    This patch allows to pass own logger, eg. Rails.logger
    or Syslog.
    
    Custom logger can be passed in initializer through options hash:
    Pubnub.new(:subscribe_key => '...', :logger => Rails.logger)
    
    If custom logger is not given it uses default logger.

--- 3.3/lib/pubnub.rb
+++ 3.3/lib/pubnub.rb
@@ -38,9 +38,6 @@ class Pubnub
   TIMEOUT_SUBSCRIBE = 310
   TIMEOUT_NON_SUBSCRIBE = 5
 
-  PUBNUB_LOGGER = Logger.new("#{Dir.tmpdir}/pubnubError.log", 10, 10000000)
-  PUBNUB_LOGGER.level = Logger::DEBUG
-
   class PresenceError < RuntimeError;
   end
   class PublishError < RuntimeError;
@@ -73,6 +70,7 @@ class Pubnub
       @secret_key = options_hash[:secret_key].blank? ? nil : options_hash[:secret_key].to_s
       @cipher_key = options_hash[:cipher_key].blank? ? nil : options_hash[:cipher_key].to_s
       @ssl = options_hash[:ssl].blank? ? false : true
+      @logger = options_hash[:logger]
 
     else
       raise(InitError, "Initialize with either a hash of options, or exactly 5 named parameters.")
@@ -278,6 +276,10 @@ class Pubnub
     end
   end
 
+  def logger
+    @logger ||= initDefaultLogger
+  end
+
   private
 
   def _request(request, is_reactor_running = false)
@@ -378,9 +380,9 @@ class Pubnub
   end
 
   def logError(errMsg, url)
-    PUBNUB_LOGGER.debug("url: #{url}")
-    PUBNUB_LOGGER.debug("#{errMsg}")
-    PUBNUB_LOGGER.debug("")
+    logger.debug("url: #{url}")
+    logger.debug("#{errMsg}")
+    logger.debug("")
   end
 
   def retryRequest(is_reactor_running, req, request, delay)
@@ -395,11 +397,17 @@ class Pubnub
       request.set_error(true)
       request.callback.call(error_msg)
 
-      PUBNUB_LOGGER.debug(error_msg)
+      logger.debug(error_msg)
 
       EM.stop unless is_reactor_running
     end
 
+  end
+
+  def initDefaultLogger
+    logger = Logger.new("#{Dir.tmpdir}/pubnubError.log", 10, 10000000)
+    logger.level = Logger::DEBUG
+    logger
   end
 
 end
