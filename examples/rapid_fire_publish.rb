902e543f66b3a351f0dc027f78137ecca8d777ec
geremy cohen <geremy@pubnub.com>
2013-07-23 16:37:05 -0700

    added new version of rapid_fire example

--- examples/rapid_fire_publish.rb
+++ examples/rapid_fire_publish.rb
@@ -2,30 +2,53 @@ require 'pubnub'
 
 class PubTest
 
-  @counter = 0;
-  @p = Pubnub.new(:subscribe_key => "demo", :publish_key => "demo")
-  @cb = lambda { |x| puts("cb1 #{x.channel}: msg: #{x.message} response: #{x.response}"); @counter+=1 }
+  MAX_CYCLES = 10
+  FREQUENCY = 0.250
 
-  @p.subscribe(:channel => "hello_world", :callback => @cb)
+  def error_callback(error)
+    puts "Error: #{error.inspect}"
+  end
 
-  sleep(2)
+  def connect_callback(x)
+    puts "Subscribe connected: #{x.inspect}"
+    puts "Commencing publishes..."
+    publish_cycle
+  end
 
+  def initialize
 
-  def self.publish_test
-    100.times do |x|
-      sleep(0.200)
+    @counter = 0;
+    @p = Pubnub.new(:subscribe_key => "demo",
+                    :publish_key => "demo",
+                    :error_callback => method(:error_callback),
+                    :connect_callback => method(:connect_callback))
+
+    @publish_cb = lambda { |x| puts("Publish #{x.channel}: msg: #{x.message} response: #{x.response}")}
+    @subscribe_cb = lambda { |x| puts("Subscribe #{x.channel}: msg: #{x.message} response: #{x.response}"); @counter+=1 }
+
+  end
+
+  def publish_cycle
+    MAX_CYCLES.times do |x|
+      sleep(FREQUENCY)
       msg = {"serial" => x}
-      @p.publish(:message => msg, :channel => "hello_world", :callback => @cb)
+      @p.publish(:message => msg, :channel => "hello_world", :callback => @publish_cb)
     end
 
-    puts "Received #{@counter}"
+    puts "Received #{@counter} of #{MAX_CYCLES}"
   end
 
 
-  publish_test
+  def subscribe
+    @p.subscribe(:channel => "hello_world", :callback => @subscribe_cb)
+  end
+
+  def self.go
+    pt = PubTest.new
+    pt.subscribe
+  end
 
 end
 
-
-
-
+pt = PubTest.new
+pt.subscribe
