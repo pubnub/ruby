25e45c5b1a3184f74cd00a9df75f4f1165ace1e3
Tomasz Weissbek <blazeroot@gmail.com>
2015-09-24 12:23:34 +0200

    removed old test.rb

--- test.rb
+++
@@ -1,9 +0,0 @@
-require 'pubnub'
-
-p = Pubnub.new(subscribe_key: :demo, publish_key: :demo)
-p.subscribe(channel: :somechannel){|e|}
-
-loop do
-  p.publish(channel: :somechannel, message: 'dupa'){|e|}
-  sleep(0.1)
-end
