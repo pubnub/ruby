c5b1a45494db6cca4ae4c9045b7500f55996a9ef
Pedro Henrique <pedrohenriqueacruz@gmail.com>
2017-11-17 17:07:50 -0200

    Extract random bytes generation to a method. (#59)
    * Extract random bytes generation to a method.
    
    Extract random bytes generation so a separated method to improve code maintainability
    
    * Update uuid.rb
    
    remove trailing whitespace for codacy

--- lib/pubnub/uuid.rb
+++ lib/pubnub/uuid.rb
@@ -4,10 +4,14 @@ module Pubnub
   module UUID
     class << self
       def generate
-        arr = Random.new.bytes(16).unpack('NnnnnN')
+        arr = random_bytes_array
         arr[2] = (arr[2] & 0x0fff) | 0x4000
         arr[3] = (arr[3] & 0x3fff) | 0x8000
         format('%08x-%04x-%04x-%04x-%04x%08x', *arr)
+      end
+
+      def random_bytes_array
+        Random.new.bytes(16).unpack('NnnnnN')
       end
     end
   end
